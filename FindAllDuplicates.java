d00c4db7b890bc472ae6398d78f14be97411fdec
Your Name <you@example.com>
2024-09-06 12:15:41 +0530

    Made a change to java file

--- FindAllDuplicates.java
+++ FindAllDuplicates.java
@@ -6,6 +6,7 @@ public class FindAllDuplicates {
 	
 	 public List<Integer> findDuplicates(int[] nums) {
 		 int i = 0;
+		 
 		 while (i< nums.length) {
 			 int correct = nums[i] - 1;
 			 if(nums[i]!= nums[correct]) {
